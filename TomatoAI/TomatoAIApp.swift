b0c725e7d6a3f1aa67a9d22c85bec3541919f961
Ulysses <ulyssesfm@gmail.com>
2022-10-22 14:28:36 -0700

    Initial Commit

---
+++ TomatoAI/TomatoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoAIApp.swift
+//  TomatoAI
+//
+//  Created by Ulysses Foote-McNabb on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TomatoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TomatoAIUITests/TomatoAIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TomatoAIUITestsLaunchTests.swift
+//  TomatoAIUITests
+//
+//  Created by Ulysses Foote-McNabb on 10/22/22.
+//
+
+import XCTest
+
+final class TomatoAIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
